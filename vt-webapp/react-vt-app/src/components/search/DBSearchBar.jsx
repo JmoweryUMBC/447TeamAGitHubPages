c63f6c150527c14d85358043450d73dd4662099c
Graymon13 <grayson.wasell@gmail.com>
2022-05-17 01:33:26 -0400

    Added additional display info to DBSearch Page
    Added displaying the searched hash number and error descriptions by case

--- vt-webapp/react-vt-app/src/components/search/DBSearchBar.jsx
+++ vt-webapp/react-vt-app/src/components/search/DBSearchBar.jsx
@@ -6,18 +6,51 @@ import { Button } from '@mui/material';
 
 const DBSearchBar = () => {
         const [Virushash, setVirushash] = useState([]);
-        const SearchVT = (Virushash) => {
+        const [hashInfo, setHashInfo] = useState([])
+        const [detectedData, setDetectedData] = useState("");
+        const [errorInfo, setErrorInfo] = useState([]);
+        let results;
+
+        const SearchDB = (Virushash) => {
           fetch(`http://localhost:5000/DBSearch/${Virushash}`)
+            .then( resp => {
+              results = resp.json()
+              console.log(results)
+
+              if(resp.status === 500) {
+                console.log("Error somewhere w/ " + Virushash)
+                results.then(value => {
+                  console.log("Error desc: " + value["error"])
+                  setErrorInfo("Error: " + value["error"])
+                  setHashInfo("")
+                  setDetectedData("")
+                })
+              } else if(resp.status === 200) {
+                console.log("Success w/ " + Virushash)
+                results.then(value => {
+                  //console.log("Results: " + value["positives"] + "/" + value["total"])
+                  setErrorInfo("")
+                  setHashInfo("Results For Hash: " + value["md5"])
+                  //setDetectedData("Malicious Detections: " + value["positives"] + "/" + value["total"])
+                })
+              }
+
+            })
         }
 
-        return (
-        <div className = 'SearchBlock'>
-            <div className = "Search_Input">
-                    <input type="text" placeholder="Enter Hash Here" value={Virushash} onChange={e => setVirushash(e.target.value)}/>
-                    <Button className = "Search_icon" endIcon= {<SearchIcon />} onClick ={() => SearchVT(Virushash)}></Button>
-            </div>
-        </div>
-    )
+  return (
+    <>
+    <p>{errorInfo}</p>
+    <div className = 'SearchBlock'>
+      <div className = "Search_Input">
+        <input type="text" placeholder="Enter Hash Here" value={Virushash} onChange={e => setVirushash(e.target.value)}/>
+        <Button className = "Search_icon" endIcon= {<SearchIcon />} onClick ={() => SearchDB(Virushash)}></Button>
+      </div>
+    </div>
+    <p>{hashInfo}</p>
+    <p>{detectedData}</p>
+    </>
+  )
 }
 
 export default DBSearchBar
