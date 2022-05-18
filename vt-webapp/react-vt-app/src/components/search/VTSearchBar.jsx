7306f1f7987ceac7e284d55185d370a7f408009a
NoobCoderKevin <kevinzheng0621@gmail.com>
2022-05-17 22:14:40 -0400

    visual update for table
    Wanted to change the table alot more but having alot of trouble accessing the root component of the material ui table

--- vt-webapp/react-vt-app/src/components/search/VTSearchBar.jsx
+++ vt-webapp/react-vt-app/src/components/search/VTSearchBar.jsx
@@ -12,12 +12,26 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+import SxProps from "@mui/material";
+import { lightBlue } from '@mui/material/colors';
+import { makeStyles } from '@material-ui/core/styles';
 
 const VTSearchBar = () => {
         const [tableCode, setTableCode] = useState();
         const [Virushash, setVirushash] = useState([]);
         const [error, setError] = useState("");
+      
+        const tableContainer: SxProps = {
+          border: '1px solid rgba(128,128,128,0.4)',
+          width: "max-content",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: 4,
+          borderRadius: 2,
+          maxHeight: 500
+        };
+        const primary = lightBlue[400];
+        const secondary = lightBlue[200];
         let results;
 
         function createRow(name, detected, type) {
@@ -32,21 +46,21 @@ const VTSearchBar = () => {
           }
 
           let tableRows = (rows.map((row) => (
-            <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row">{row.name}</TableCell>
-              <TableCell align="right">{row.detected}</TableCell>
-              <TableCell align="right">{row.type}</TableCell>
+            <TableRow key={row.name} sx={{ '&:last-child td, &:last-child th': { border: 0 }}}>
+              <TableCell component="th" scope="row" sx={{backgroundColor: primary, borderBottom: "1px solid lightblue"  }}>{row.name} </TableCell>
+              <TableCell align="center" sx={{backgroundColor: primary, borderBottom: "1px solid lightblue"}}>{row.detected} </TableCell>
+              <TableCell align="right" sx={{backgroundColor: primary, borderBottom: "1px solid lightblue" }}>{row.type} </TableCell>
             </TableRow>
           )))
 
             setTableCode(
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableContainer component={Paper} sx={tableContainer}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table" stickyHeader={true}>
                   <TableHead>
-                    <TableRow>
-                      <TableCell>Provider</TableCell>
-                      <TableCell align="right">Detected</TableCell>
-                      <TableCell align="right">Type</TableCell>
+                    <TableRow >
+                      <TableCell  sx={{backgroundColor: secondary, borderBottom: "0px", fontWeight: 'bold', fontSize: '25px'}} align="left">Provider</TableCell>
+                      <TableCell  sx={{backgroundColor: secondary, borderBottom: "0px", fontWeight: 'bold', fontSize: '25px'}} align="center">Virus Detected</TableCell>
+                      <TableCell  sx={{backgroundColor: secondary, borderBottom: "0px", fontWeight: 'bold', fontSize: '25px'}} align="right">Type</TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
@@ -56,7 +70,6 @@ const VTSearchBar = () => {
               </TableContainer>
             )
         }
-        
         const SearchVT = (Virushash) => {
           fetch(`http://localhost:5000/VTSearch/${Virushash}`)
           .then( resp => {
@@ -77,10 +90,6 @@ const VTSearchBar = () => {
               console.log("Success w/ " + Virushash)
               results.then(value => {
                 console.log("Results: " + value["positives"] + "/" + value["total"])
-              })
-
-              //Call displaying successful data on page
-              results.then(value => {
                 updateTableRows(value)
               })
 
@@ -96,7 +105,9 @@ const VTSearchBar = () => {
                     <Button className = "Search_icon" endIcon= {<SearchIcon />} onClick ={() => SearchVT(Virushash)}></Button>
                   </div>
                 </div>
-                {tableCode}
+                <div className = "Reacttable">
+                  {tableCode}
+                </div>
           </>
         )
 }
